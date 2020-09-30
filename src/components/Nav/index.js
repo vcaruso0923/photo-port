792e8ec3561c5dc17c36884d1d4b2df88e92e918
vcaruso <vinnycar0923@gmail.com>
2020-09-30 12:31:05 -0700

    tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -28,21 +28,23 @@ function Nav() {
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">About me</a>
+                        <a data-testid="about" href="#about">
+                            About me
+                        </a>
                     </li>
                     <li>
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li className="mx-1" key={category.name}>
-                                <span onClick={() => categorySelected(category.name)}>
+                            <span onClick={() => categorySelected(category.name)}>
                                 {category.name}
                             </span>
                         </li>
